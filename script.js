a0476778f32edabc48d5c35e987d7cdc56c4e8a9
sgrscsurya <sgrscsurya@gmail.com>
2026-05-26 18:56:12 +0530

    Removed the Add to Cart animation effect for now, and implemented the quantity count function

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let categoryBtnsContainer = document.getElementById("category_btns")
 let productContainer = document.getElementById("products_container")
+let allProducts = []
 let fetchData = async () => {
-	let api = await fetch("https://dummyjson.com/products?limit=194")
-	let response = await api.json()
-	let finalData = response.products
-	displayProducts(finalData)
+let api = await fetch("https://dummyjson.com/products?limit=194")
+let response = await api.json()
+let finalData = response.products
+allProducts = finalData
+displayProducts(finalData)
 
 	let categories = finalData.map((product) => {
 		return product.category
@@ -99,50 +101,27 @@ function displayProducts(finalData) {
 }
 
 function addToCart(id) {
-	let allProductsCartBtns = document.querySelectorAll(".product_cart")
-	allProductsCartBtns.forEach((productCartBtn) => {
-		let productCartBtnId = Number(productCartBtn.dataset.id)
-		if (productCartBtnId === id) {
-			productCartBtn.classList.add("added")
-			productCartBtn.innerHTML = `
-				<svg 
-					xmlns="http://www.w3.org/2000/svg" 
-					width="24" 
-					height="24" 
-					viewBox="0 0 24 24" 
-					fill="none" 
-					stroke="currentColor" 
-					stroke-width="3" 
-					stroke-linecap="round" 
-					stroke-linejoin="round"
-				>
-					<path d="M20 6 9 17l-5-5"/>
-				</svg>
-			`
-
-			setTimeout(() => {
-				productCartBtn.classList.remove("added")
-				productCartBtn.innerHTML = `
-					<svg 
-						xmlns="http://www.w3.org/2000/svg" 
-						width="24" 
-						height="24" 
-						viewBox="0 0 24 24" 
-						fill="none" 
-						stroke="currentColor" 
-						stroke-width="2" 
-						stroke-linecap="round" 
-						stroke-linejoin="round" 
-						class="lucide lucide-shopping-bag-icon lucide-shopping-bag"
-					>
-						<path d="M16 10a4 4 0 0 1-8 0"/>
-						<path d="M3.103 6.034h17.794"/>
-						<path d="M3.4 5.467a2 2 0 0 0-.4 1.2V20a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6.667a2 2 0 0 0-.4-1.2l-2-2.667A2 2 0 0 0 17 2H7a2 2 0 0 0-1.6.8z"/>
-					</svg>
-				`
-			}, 2000)
-		}
+	let clickedProduct = allProducts.find(product => product.id === id)
+	console.log(clickedProduct)
+	let cartItems = JSON.parse(localStorage.getItem("cart")) || []
+	let existingProduct = cartItems.find((product) => {
+		return product.id === id
 	})
+	if(existingProduct){
+		clickedProduct.qty=existingProduct.qty++
+	}
+	else 
+	{
+		let productObj = {
+			id: clickedProduct.id,
+			title: clickedProduct.title,
+			img : clickedProduct.images[0],
+			qty:1,
+			price: Number((clickedProduct.price - (clickedProduct.price * (clickedProduct.discountPercentage/100))).toFixed(2))
+		}
+	cartItems.push(productObj)
+	}
+localStorage.setItem("cart",JSON.stringify(cartItems))
 }
 
 fetchData()
